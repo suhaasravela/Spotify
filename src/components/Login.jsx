a80b0b93ee23b3df5e239f3d95392c3d8a934b61
Suhaas Ravela <suhaasravela@gmail.com>
2023-08-13 13:04:37 -0400

    Login Page

---
+++ src/components/Login.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Login() {
+    const handleClick = () => {
+        const clientId = "88fff968a9a5494b8ca5a50fae789425";
+        const redirectUrl = "http://localhost:3000/";
+        const apiUrl = "https://accounts.spotify.com/authorize";
+        const scope = [
+        'user-read-email', 
+        'user-read-private',
+        'user-read-playback-state',
+        'user-modify-playback-state',
+        'user-read-currently-playing',
+        'user-read-playback-position',
+        'user-top-read',
+        'user-read-recently-played'
+    ];
+    window.location.href = `${apiUrl}?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope.join(
+        " "
+        )}&response_type=token&show_dialog=true`;
+    };
+  return (
+    <Container>
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
+        alt="spotify"
+      />
+              <button onClick={handleClick}>Connect Spotify</button>
+
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  background-color: #1db954;
+  gap: 5rem;
+  img {
+    height: 20vh;
+  }
+  button {
+    padding: 1rem 5rem;
+    border-radius: 5rem;
+    background-color: black;
+    color: #49f585;
+    border: none;
+    font-size: 1.4rem;
+    cursor: pointer;
+  }
+`;
