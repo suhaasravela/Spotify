d0606b94401321057cb35225f261c62457ae2224
Suhaas Ravela <suhaasravela@gmail.com>
2023-08-14 00:52:32 -0400

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,13 @@ export default function Footer() {
 }
 
 const Container = styled.div`
-    background-color: #181818; //#181818
-    height: 100%
+  height: 100%;
+  width: 100%;
+  background-color: black;
+  border-top: 1px solid #282828;
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  align-items: center;
+  justify-content: center;
+  padding: 0 1rem;
 `;
